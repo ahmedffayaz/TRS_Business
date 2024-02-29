f6a1d0043cfd4df2c55872ecc0346a46a835e3c3
M Umair <muhammad.umair@therightsw.com>
2024-02-29 10:38:37 +0500

    Add knowledge base permissions in blade

--- resources/views/livewire/backend/knowledge-base/knowledge-base-data-component.blade.php
+++ resources/views/livewire/backend/knowledge-base/knowledge-base-data-component.blade.php
@@ -1,16 +1,18 @@
 <div>
     @section('breadcrumbs-button')
-        <div class="d-flex">
-            <div class="col-md-7">
-                <x-anchor-tag class="btn btn-primary" href="javascript:void(0);"
-                    :value="__('Add Question')" tabindex="0" aria-controls="table-hover"
-                    type="button" wire:click="openCreateKnowledgeBaseModal" wire:ignore />
+        @can('add_knowledgeBase')
+            <div class="d-flex">
+                <div class="col-md-7">
+                    <x-anchor-tag class="btn btn-primary" href="javascript:void(0);"
+                        :value="__('Add Question')" tabindex="0" aria-controls="table-hover"
+                        type="button" wire:click="openCreateKnowledgeBaseModal" wire:ignore />
+                </div>
+                <div class="col-md-5">
+                    <x-anchor-tag class="btn btn-primary" href="javascript:void(0);" :value="__('Add Category')"
+                        tabindex="0" aria-controls="table-hover" type="button" wire:click="openKnowledgeBaseCategoryModal" wire:ignore />
+                </div>
             </div>
-            <div class="col-md-5">
-                <x-anchor-tag class="btn btn-primary" href="javascript:void(0);" :value="__('Add Category')"
-                    tabindex="0" aria-controls="table-hover" type="button" wire:click="openKnowledgeBaseCategoryModal" wire:ignore />
-            </div>
-        </div>
+        @endcan
     @endsection
 
     <div class="row mb-2">
@@ -24,100 +26,131 @@
     </div>
     <div class="row kb-search-content-info match-height">
         @forelse ($knowledgeBaseCategories as $category)
-            <div class="col-md-4 col-sm-6 col-12 kb-search-content">
-                <!-- account setting card -->
-                <div class="card">
-                    <div class="card-header">
-                        <span wire:ignore.>
-                            <i data-feather="settings" class="font-medium-4 me-50 text-primary"></i>
-                        </span>
-                        <h4 class="card-title">{{ strlen($category->name) > 30
-                            ? substr($category->name, 0, 30) . '...'
-                            : $category->name }}</h4>
-                        <div class="dropdown">
-                            <button type="button" class="btn btn-sm dropdown-toggle hide-arrow py-0"
-                                data-bs-toggle="dropdown">
-                                <span wire:ignore.>
-                                    <i data-feather="more-vertical"></i>
-                                </span>
-                            </button>
-                            <div class="dropdown-menu dropdown-menu-end">
-                                <x-anchor-tag class="dropdown-item" href="javascript:void(0);"
-                                    wire:click="edit({{ $category->id }})">
+            @can('view_knowledgeBase')
+                <div class="col-md-4 col-sm-6 col-12 kb-search-content">
+                    <!-- account setting card -->
+                    <div class="card">
+                        <div class="card-header">
+                            <span wire:ignore.>
+                                <i data-feather="settings" class="font-medium-4 me-50 text-primary"></i>
+                            </span>
+                            <h4 class="card-title">{{ strlen($category->name) > 30
+                                ? substr($category->name, 0, 30) . '...'
+                                : $category->name }}</h4>
+                            @can('edit_knowledgeBase', 'delete_knowledgeBase')
+                                <div class="dropdown">
+                                    <button type="button" class="btn btn-sm dropdown-toggle hide-arrow py-0"
+                                        data-bs-toggle="dropdown">
+                                        <span wire:ignore.>
+                                            <i data-feather="more-vertical"></i>
+                                        </span>
+                                    </button>
+                                    <div class="dropdown-menu dropdown-menu-end">
+                                        @can('edit_knowledgeBase')
+                                            <x-anchor-tag class="dropdown-item" href="javascript:void(0);"
+                                                wire:click="edit({{ $category->id }})">
+                                                <span wire:ignore.>
+                                                    <i data-feather="edit-2" class="me-50"></i>
+                                                </span>
+                                                <span>Edit</span>
+                                            </x-anchor-tag>
+                                        @endcan
+                                        @can('delete_knowledgeBase')
+                                            <x-anchor-tag class="dropdown-item delete" href="javascript:void(0)"
+                                                wire:click="deleteCategoryConfirmation({{ $category->id }})">
+                                                <span wire:ignore.>
+                                                    <i data-feather="trash" class="me-50"></i>
+                                                </span>
+                                                <span>Delete</span>
+                                            </x-anchor-tag>
+                                        @endcan
+                                    </div>
+                                </div>
+                            @else
+                                <div class="dropdown">
+                                    <button type="button" class="btn btn-sm dropdown-toggle hide-arrow py-0"
+                                        data-bs-toggle="dropdown">
                                     <span wire:ignore.>
-                                        <i data-feather="edit-2" class="me-50"></i>
+                                        <i data-feather='lock'></i>
                                     </span>
-                                    <span>Edit</span>
-                                </x-anchor-tag>
-                                <x-anchor-tag class="dropdown-item delete" href="javascript:void(0)"
-                                    wire:click="deleteCategoryConfirmation({{ $category->id }})">
-                                    <span wire:ignore.>
-                                        <i data-feather="trash" class="me-50"></i>
-                                    </span>
-                                    <span>Delete</span>
-                                </x-anchor-tag>
+                                    </button>
+                                </div>
+                            @endcan
+                        </div>
+                        <div class="card-body">
+                            <div class="list-group list-group-circle mt-2">
+                                @forelse ($category->questions as $question)
+                                    <div class="row">
+                                        <div class="col-md-10">
+                                            <x-anchor-tag href="javascript:void(0)" wire:click="showKnowledgeBase({{ $question->id . ', ' . $category->id }})">
+                                                <span class="list-group-item text-body">{{ $question->question }}</span>
+                                            </x-anchor-tag>
+                                            @if (count($question->keywords) > 0)
+                                                <span><b>Keywords:</b>
+                                                    @foreach ($question->keywords as $keyword)
+                                                        <x-anchor-tag href="{{ route('dashboard.knowledgebase.search-keyword', $keyword->name) }}">
+                                                            {{ $keyword->name }}
+                                                        </x-anchor-tag>
+                                                        @if (!$loop->last),&nbsp;@endif
+                                                    @endforeach
+                                                </span>
+                                            @endif
+                                        </div>
+                                        <div class="col-md-2">
+                                            @can('edit_knowledgeBase', 'delete_knowledgeBase')
+                                                <div class="dropdown">
+                                                    <button type="button" class="btn btn-sm dropdown-toggle hide-arrow py-0"
+                                                    data-bs-toggle="dropdown">
+                                                        <span wire:ignore.>
+                                                            <i data-feather="more-vertical"></i>
+                                                        </span>
+                                                    </button>
+                                                    <div class="dropdown-menu dropdown-menu-end">
+                                                        @can('edit_knowledgeBase')
+                                                            <x-anchor-tag class="dropdown-item" href="javascript:void(0);"
+                                                            wire:click="editKnowledgeBase({{ $question->id }}, {{ $category->id }})">
+                                                                <span wire:ignore.>
+                                                                    <i data-feather="edit-2" class="me-50"></i>
+                                                                </span>
+                                                                <span>Edit</span>
+                                                            </x-anchor-tag>
+                                                        @endcan
+                                                        @can('delete_knowledgeBase')
+                                                            <x-anchor-tag class="dropdown-item delete" href="javascript:void(0);"
+                                                            wire:click="deleteKnowledgeBaseConfirmation({{ $question->id }})">
+                                                                <span wire:ignore.>
+                                                                    <i data-feather="trash" class="me-50"></i>
+                                                                </span>
+                                                                <span>Delete</span>
+                                                            </x-anchor-tag>
+                                                        @endcan
+                                                    </div>
+                                                </div>
+                                            @else
+                                                <div class="dropdown">
+                                                    <button type="button" class="btn btn-sm dropdown-toggle hide-arrow py-0"
+                                                    data-bs-toggle="dropdown">
+                                                        <span wire:ignore.>
+                                                            <i data-feather='lock'></i>
+                                                        </span>
+                                                    </button>
+                                                </div>
+                                            @endcan
+                                        </div>
+                                    </div>
+                                @empty
+                                    <div class="row">
+                                        <div class="col-md-12 text-center">
+                                            <p>No record found!</p>
+                                        </div>
+                                    </div>
+                                @endforelse
                             </div>
                         </div>
                     </div>
-                    <div class="card-body">
-                        <div class="list-group list-group-circle mt-2">
-                            @forelse ($category->questions as $question)
-                                <div class="row">
-                                    <div class="col-md-10">
-                                        <x-anchor-tag href="javascript:void(0)" wire:click="showKnowledgeBase({{ $question->id . ', ' . $category->id }})">
-                                            <span class="list-group-item text-body">{{ $question->question }}</span>
-                                        </x-anchor-tag>
-                                        @if (count($question->keywords) > 0)
-                                            <span><b>Keywords:</b>
-                                                @foreach ($question->keywords as $keyword)
-                                                    <x-anchor-tag href="{{ route('dashboard.knowledgebase.search-keyword', $keyword->name) }}">
-                                                        {{ $keyword->name }}
-                                                    </x-anchor-tag>
-                                                    @if (!$loop->last),&nbsp;@endif
-                                                @endforeach
-                                            </span>
-                                        @endif
-                                    </div>
-                                    <div class="col-md-2">
-                                        <div class="dropdown">
-                                            <button type="button" class="btn btn-sm dropdown-toggle hide-arrow py-0"
-                                            data-bs-toggle="dropdown">
-                                                <span wire:ignore.>
-                                                    <i data-feather="more-vertical"></i>
-                                                </span>
-                                            </button>
-                                            <div class="dropdown-menu dropdown-menu-end">
-                                                <x-anchor-tag class="dropdown-item" href="javascript:void(0);"
-                                                wire:click="editKnowledgeBase({{ $question->id }}, {{ $category->id }})">
-                                                    <span wire:ignore.>
-                                                        <i data-feather="edit-2" class="me-50"></i>
-                                                    </span>
-                                                    <span>Edit</span>
-                                                </x-anchor-tag>
-                                                <x-anchor-tag class="dropdown-item delete" href="javascript:void(0);"
-                                                wire:click="deleteKnowledgeBaseConfirmation({{ $question->id }})">
-                                                    <span wire:ignore.>
-                                                        <i data-feather="trash" class="me-50"></i>
-                                                    </span>
-                                                    <span>Delete</span>
-                                                </x-anchor-tag>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            @empty
-                                <div class="row">
-                                    <div class="col-md-12 text-center">
-                                        <p>No record found!</p>
-                                    </div>
-                                </div>
-                            @endforelse
-                        </div>
-                    </div>
                 </div>
-            </div>
+            @endcan
         @empty
-            <!-- no result -->
             <div class="col-md-12 text-center kb-search-content">
                 <h4 class="mt-4">Search result not found!!</h4>
             </div>
