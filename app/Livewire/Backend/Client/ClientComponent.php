d0e15a9b1f4411c4b5c684830cefc19b75dfaa54
M Umair <muhammad.umair@therightsw.com>
2024-03-20 11:23:40 +0500

    Display clients to user if has permission

--- app/Livewire/Backend/Client/ClientComponent.php
+++ app/Livewire/Backend/Client/ClientComponent.php
@@ -44,20 +44,14 @@ class ClientComponent extends Component
     {
         $user = auth()->user();
         $this->search ? $this->resetPage() : ''; // reset pagination while searching
-        $clients = Client::whereHas('business', function ($query) {
+        return Client::whereHas('business', function ($query) {
             $query->whereName(session('business'));
-        })->when($user->hasRole('client'), function ($query) use ($user) {
+        })->when($user->hasPermissionTo('view_clients') && $user->hasRole('client')
+        && !$user->hasRole('super-admin'), function ($query) use ($user) {
             $query->whereHas('employees', function ($query) use ($user) {
                 $query->whereId($user->id);
             });
-        });
-
-        if ($user->hasPermissionTo('view_clients') && !$user->hasRole('super-admin')) {
-            $clients = $clients;
-        }
-
-
-        return $clients->with(['business', 'country', 'employees'])->withCount('employees')
+        })->with(['business', 'country', 'employees'])->withCount('employees')
             ->getList($this->search, $this->columnName, $this->sortDirection)
             ->paginate($this->limitPerPage);
     }
