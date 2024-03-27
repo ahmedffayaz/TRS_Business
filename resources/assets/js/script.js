cae4c66816cb8abd387a09445bc44d541358f866
M Umair <muhammad.umair@therightsw.com>
2024-03-27 10:44:29 +0500

    resole accept terms conditions page bug, display version and business name, add digital signature pad

--- resources/assets/js/script.js
+++ resources/assets/js/script.js
@@ -125,8 +125,8 @@ document.addEventListener("livewire:initialized", () => {
 
     Livewire.on('editor-value-set', function (data) {
         // Populate hidden form field with Quill editor content
-        let desc = document.querySelector('input[name=description]'); console.log(desc);
-        desc.value = quill.root.innerHTML; console.log(desc.value);
+        let desc = document.querySelector('input[name=description]');
+        desc.value = quill.root.innerHTML;
     });
 
     Livewire.on('initialize-markup-editor', function (data) {
@@ -137,7 +137,13 @@ document.addEventListener("livewire:initialized", () => {
         // let textarea = data['textarea'];
         $('textarea[name="'+data['textarea']+'"').data('converter', converter);
 
-        $('.fa-header').addClass('fa-heading');
+        if (data['heading'] == false) {
+            // Hide the editor toolbar if heading is false
+        $('.editor-toolbar').css('display', 'none');
+        } else {
+            // Add class to heading icon
+            $('.fa-header').addClass('fa-heading');
+        }
     });
 
     Livewire.on('markup-editor-change', function (data) {
@@ -148,5 +154,172 @@ document.addEventListener("livewire:initialized", () => {
 
     Livewire.on('flatpickr', function (data) {
         flatpickr('.flatpickr-basic');
-    })
+    });
+
+    Livewire.on('digital-signature-pad', function (data) {
+        window.requestAnimFrame = (function(callback) {
+            return window.requestAnimationFrame ||
+                window.webkitRequestAnimationFrame ||
+                window.mozRequestAnimationFrame ||
+                window.oRequestAnimationFrame ||
+                window.msRequestAnimaitonFrame ||
+                function(callback) {
+                    window.setTimeout(callback, 1000 / 60);
+                };
+        })();
+
+        var canvas = document.getElementById(data['canvas_id']);
+        var ctx = canvas.getContext("2d");
+        ctx.strokeStyle = data['ctx_strokes_style'];
+        ctx.lineWidth = data['ctx_line_width'];
+
+        var drawing = false;
+        var mousePos = {x: 0, y: 0};
+        var lastPos = mousePos;
+
+        canvas.addEventListener("mousedown", function(e) {
+            drawing = true;
+            lastPos = getMousePos(canvas, e);
+        }, false);
+
+        canvas.addEventListener("mouseup", function(e) {
+            drawing = false;
+        }, false);
+
+        canvas.addEventListener("mousemove", function(e) {
+            mousePos = getMousePos(canvas, e);
+        }, false);
+
+        // Add touch event support for mobile
+        canvas.addEventListener("touchstart", function(e) {
+
+        }, false);
+
+        canvas.addEventListener("touchmove", function(e) {
+            var touch = e.touches[0];
+            var me = new MouseEvent("mousemove", {
+                clientX: touch.clientX,
+                clientY: touch.clientY
+            });
+            canvas.dispatchEvent(me);
+        }, false);
+
+        canvas.addEventListener("touchstart", function(e) {
+            mousePos = getTouchPos(canvas, e);
+            var touch = e.touches[0];
+            var me = new MouseEvent("mousedown", {
+                clientX: touch.clientX,
+                clientY: touch.clientY
+            });
+            canvas.dispatchEvent(me);
+        }, false);
+
+        canvas.addEventListener("touchend", function(e) {
+            var me = new MouseEvent("mouseup", {});
+            canvas.dispatchEvent(me);
+        }, false);
+
+        function getMousePos(canvasDom, mouseEvent) {
+            var rect = canvasDom.getBoundingClientRect();
+            return {
+                x: mouseEvent.clientX - rect.left,
+                y: mouseEvent.clientY - rect.top
+            }
+        }
+
+        function getTouchPos(canvasDom, touchEvent) {
+            var rect = canvasDom.getBoundingClientRect();
+            return {
+                x: touchEvent.touches[0].clientX - rect.left,
+                y: touchEvent.touches[0].clientY - rect.top
+            }
+        }
+
+        function renderCanvas() {
+            if (drawing) {
+                ctx.moveTo(lastPos.x, lastPos.y);
+                ctx.lineTo(mousePos.x, mousePos.y);
+                ctx.stroke();
+                lastPos = mousePos;
+            }
+        }
+
+        // Prevent scrolling when touching the canvas
+        document.body.addEventListener("touchstart", function(e) {
+            if (e.target == canvas) {
+                e.preventDefault();
+            }
+        }, false);
+
+        document.body.addEventListener("touchend", function(e) {
+            if (e.target == canvas) {
+                e.preventDefault();
+            }
+        }, false);
+
+        document.body.addEventListener("touchmove", function(e) {
+            if (e.target == canvas) {
+                e.preventDefault();
+            }
+        }, false);
+
+        (function drawLoop() {
+            requestAnimFrame(drawLoop);
+            renderCanvas();
+        })();
+
+        function clearCanvas() {
+            canvas.width = canvas.width;
+        }
+
+        // Set up the UI
+        var sigText = document.getElementById("sig-dataUrl");
+        var sigImage = document.getElementById("sig-image");
+        var clearButton = document.getElementById(data['clear_button_id']);
+        var submitButton = document.getElementById(data['sign_submit_button_id']);
+
+        clearButton.addEventListener("click", function(event) {
+            event.preventDefault();
+            clearCanvas();
+        }, false);
+
+        submitButton.addEventListener("click", function(event) {
+            event.preventDefault()
+            var dataUrl = canvas.toDataURL();
+
+            canvas.toBlob(function(blob) {
+
+            var fd = new FormData($(data['form_id'])[0]);
+            fd.append('digital_signature', blob);
+
+            var $form = $('.report-form');
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+
+            $.ajax({
+                type: 'POST',
+                url: data['url'],
+                data: fd,
+                contentType: false,
+                cache: false,
+                processData: false,
+                success: function(response) {
+                    $('input[name="' + data['digital_signature_pad'] + '"]').val(response.data.signature);
+                        // @this.set('form.digital_signature_pad', response.data.signature);
+                    $('.user-sign').attr('src', data['storage_path'] + '/' + response.data.signature);
+                    clearCanvas();
+                    Livewire.dispatch('close-main-modal');
+
+                },
+                error: function(response) {
+
+                }
+            });
+            }, "image/png");
+
+        }, false);
+    });
 });
--- resources/views/components/input-checkbox.blade.php
+++ resources/views/components/input-checkbox.blade.php
@@ -1,6 +1,6 @@
-@props(['labelValue', 'id', 'statusClass' => null, 'value' => null])
+@props(['labelValue', 'id', 'statusClass' => null, 'value' => null, 'isRequired' => false])
 
 <div class="form-check {{ $statusClass ? $statusClass : 'form-check-inline' }}">
     <input {{ $attributes->merge(['class' => 'form-check-input']) }} id="{{ $id }}" @if ($value) value="{{ $value }}" @endif />
-    <x-input-label class="form-check-label" for="{{ $id }}" :value="$labelValue" />
+    <x-input-label class="form-check-label {{ $isRequired ? 'required' : '' }}" for="{{ $id }}" :value="$labelValue" />
 </div>
