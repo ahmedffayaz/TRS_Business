753cda2d6b9478c689e98ba2612a3a6007b53357
Ahmed Fayyaz <ahmad.fayyaz@therightsw.com>
2024-06-27 19:00:08 +0500

    Storing hours through chat

--- app/Livewire/Backend/Comment/ChatComponent.php
+++ app/Livewire/Backend/Comment/ChatComponent.php
@@ -5,11 +5,23 @@ namespace App\Livewire\Backend\Comment;
 use App\Models\Task;
 use App\Models\Comment;
 use Livewire\Component;
+use Exception;
+use Validator;
+use App\Enums\Comment\CommentType;
+use App\Enums\Comment\CommentUnit;
+use App\Livewire\Forms\ChatHourForm;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Pagination\LengthAwarePaginator;
 
 class ChatComponent extends Component
 {
     public ?int $taskId;
+    public $isHourModalVisible = false;
+
+    public ChatHourForm $form;
+    public $message;
+
 
     public function mount($taskId)
     {
@@ -24,7 +36,52 @@ class ChatComponent extends Component
             });
         })->with('fromUser')->get();
     }
+    public function showElement()
+    {
+        $this->isHourModalVisible = true;
+        $this->form->isHourModalVisible = true;
+        $this->dispatch('reinitialize-dispatcher');
+    }
 
+    public function hideElement()
+    {
+        $this->isHourModalVisible = false;
+        $this->form->isHourModalVisible = false;
+        $this->form->reset();
+        // $this->resetValidation();
+        $this->dispatch('reinitialize-dispatcher');
+    }
+    public function storeChatHours()
+    {
+        $validated = $this->form->validate();
+        $message = 'comment created successfully.';
+        // dd($this->form->all());
+
+        $this->isHourModalVisible = true;
+        try {
+            DB::beginTransaction();
+            Comment::create([
+                'description' => $validated['description'],
+                'time' => $validated['time'] ?? null,
+                'type' => 'comment',
+                'task_id' => $this->taskId,
+                'from' => auth()->user()->id,
+                'dated' => $validated['dated'] ?? null,
+                'is_billable' => $this->validated['is_billable'] ?? 0,
+            ]);
+
+
+            DB::commit();
+            $this->message ="";
+            $this->dispatch('alert', ['type' => 'success', 'message' => $message]);
+            // $this->showElement();
+            $this->form->reset();
+        } catch (Exception $exception) {
+            DB::rollBack();
+            Log::error('Get error while add time: ' . $exception->getMessage());
+            $this->dispatch('alert', ['type' => 'error', 'message' => 'Something went wrong.']);
+        }
+    }
     private function getTask()
     {
         return Task::whereHas('project', function ($query) {
--- resources/views/livewire/backend/comment/chat-component.blade.php
+++ resources/views/livewire/backend/comment/chat-component.blade.php
@@ -1,16 +1,37 @@
 @assets
-    <style>
-        html .navbar-floating.footer-static .app-content .content-area-wrapper,
-        html .navbar-floating.footer-static .app-content .kanban-wrapper {
-            min-height: 550px;
-            height: calc(var(--vh, 1vh) * 100 - calc(calc(2rem * 1) + 4.45rem + 18.35rem + 1.3rem + 0rem));
-        }
-    </style>
+<style>
+    html .navbar-floating.footer-static .app-content .content-area-wrapper,
+    html .navbar-floating.footer-static .app-content .kanban-wrapper {
+        min-height: 550px;
+        height: calc(var(--vh, 1vh) * 100 - calc(calc(2rem * 1) + 4.45rem + 18.35rem + 1.3rem + 0rem));
+    }
+
+    .user-chats {
+        overflow-y: auto;
+    }
+
+    .chats {
+        height: 100px;
+    }
+
+    .error-list {
+        list-style: none;
+    }
+
+    .inputs {
+        padding: 10px;
+    }
+
+    .has-error input,
+    .has-error select,
+    .has-error textarea {
+        border-color: red;
+    }
+</style>
 @endassets
 <div>
     <div class="chat-application">
         <div class="content-area-wrapper container-xxl p-0">
-            {{-- <div class="content-right"> --}}
             <div class="content-wrapper container-xxl p-0">
                 <div class="content-body">
                     <div class="body-content-overlay"></div>
@@ -25,11 +46,11 @@
                                         <div class="design-group">
                                             <div class="avatar avatar-border m-0 me-1">
                                                 @if ($task?->user?->avatar)
-                                                    <img src="{{ getUserAvatar($task?->user?->avatar) }}" alt="avatar"
-                                                        height="36" width="36" />
-                                                    <span class="avatar-status-busy"></span>
+                                                <img src="{{ getUserAvatar($task?->user?->avatar) }}" alt="avatar"
+                                                    height="36" width="36" />
+                                                <span class="avatar-status-busy"></span>
                                                 @else
-                                                    <div class="avatar-content">{{ $task?->user?->avatarName }}</div>
+                                                <div class="avatar-content">{{ $task?->user?->avatarName }}</div>
                                                 @endif
                                             </div>
                                         </div>
@@ -43,26 +64,29 @@
                             <div class="user-chats">
                                 <div class="chats">
                                     @foreach ($comments as $comment)
-                                        <div class="chat {{ $comment?->fromUser?->id !== auth()->user()->id ? 'chat-left' : '' }}">
-                                            <div class="chat-avatar">
-                                                <span class="avatar box-shadow-1 cursor-pointer">
-                                                    @if ($comment?->fromUser?->avatar)
-                                                    <img src="{{ getUserAvatar($comment?->fromUser?->avatar) }}"
-                                                        alt="avatar" height="36" width="36" />
-                                                    @else
-                                                        <div class="avatar-content">{{ $comment?->fromUser?->avatarName }}</div>
-                                                    @endif
-                                                </span>
+                                    <div
+                                        class="chat {{ $comment?->fromUser?->id !== auth()->user()->id ? 'chat-left' : '' }}">
+                                        <div class="chat-avatar">
+                                            <span class="avatar box-shadow-1 cursor-pointer">
+                                                @if ($comment?->fromUser?->avatar)
+                                                <img src="{{ getUserAvatar($comment?->fromUser?->avatar) }}"
+                                                    alt="avatar" height="36" width="36" />
+                                                @else
+                                                <div class="avatar-content">{{ $comment?->fromUser?->avatarName }}
+                                                </div>
+                                                @endif
+                                            </span>
+                                        </div>
+                                        <div class="chat-body">
+                                            <div class="chat-content">
+                                                <p class="mb-1">{{ $comment?->description }}</p>
+                                                <small><b>Created at:</b> {{ formatDate($comment?->created_at)
+                                                    }}</small>
                                             </div>
-                                            <div class="chat-body">
-                                                <div class="chat-content">
-                                                    <p class="mb-1">{{ $comment?->description }}</p>
-                                                    <small><b>Created at:</b> {{ formatDate($comment?->created_at) }}</small>
-                                                </div>
-                                                <div>
-                                                </div>
+                                            <div>
                                             </div>
                                         </div>
+                                    </div>
                                     @endforeach
                                     <div class="divider">
                                         <div class="divider-text">Yesterday</div>
@@ -72,24 +96,89 @@
                             <!-- User Chat messages -->
 
                             <!-- Submit Chat form -->
-                            <form class="chat-app-form" action="javascript:void(0);" onsubmit="enterChat();">
+                            <form class="chat-app-form position-relative" wire:submit.prevent="storeChatHours">
                                 <div class="input-group input-group-merge me-1 form-send-message">
-                                    <span class="time input-group-text" wire:click="openModal">
-                                        <i data-feather="clock" class="cursor-pointer"></i>
-                                    </span>
-                                    <input type="text" class="form-control message"
+                                    @if($isHourModalVisible)
+                                        <span class="time input-group-text" wire:click="hideElement" data-bs-toggle="collapse"
+                                            href="#collapseShowElement" role="button" aria-expanded="false" wire:ignore.
+                                            aria-controls="collapseShowElement">
+                                            <i data-feather="arrow-down" class="cursor-pointer"></i>
+                                        </span>
+                                    @endif
+                                    @if(!$isHourModalVisible)
+                                        <span class="time input-group-text" data-bs-toggle="collapse" wire:click="showElement"
+                                            href="#collapseShowElement" role="button" aria-expanded="false"  wire:ignore.
+                                            aria-controls="collapseShowElement">
+                                            <i data-feather="clock" class="cursor-pointer"></i>
+                                        </span>
+                                    @endif
+                                    <input wire:model="form.description" type="text" class="form-control message"
                                         placeholder="Type your message or use speech to text" />
-                                    <span class="input-group-text">
+                                    <span class="input-group-text" wire:ignore.>
                                         <label for="attach-doc" class="attachment-icon form-label mb-0">
                                             <i data-feather="image" class="cursor-pointer text-secondary"></i>
                                             <input type="file" id="attach-doc" hidden />
                                         </label>
                                     </span>
                                 </div>
-                                <button type="button" class="btn btn-primary send" onclick="enterChat();">
+                                <button type="submit" class="btn btn-primary send" role="button">
                                     <i data-feather="send" class="d-lg-none"></i>
                                     <span class="d-none d-lg-block">Send</span>
                                 </button>
+
+                                <div class="card collapse position-absolute bottom-50 me-2 no-shadow"
+                                    style="margin-right: -1rem !important; margin-left: -1rem; margin-bottom: 28px; box-shadow: none !important; border-radius:0%"
+                                    id="collapseShowElement">
+                                    <div class="card-body py-0 mt-1 me-2">
+                                        <div>
+                                            @php
+                                            $minutes = \App\Enums\Comment\CommentUnit::MINUTES->value;
+                                            $hours = \App\Enums\Comment\CommentUnit::HOURS->value;
+                                            @endphp
+                                            <div class="row">
+                                                <div class="col-md-3 inputs wire:ignore.">
+                                                    <x-input type="hidden" wire:model="form.task_id" />
+                                                    <x-input type="text" name="time" id="time"
+                                                        :class="$errors->has('form.time') ? 'error' : ''"
+                                                        placeholder="Enter time" wire:model="form.time" />
+                                                    @error('form.time')
+                                                    <x-input-error :message="$message" />
+                                                    @enderror
+                                                </div>
+                                                <div class="col-md-3 inputs">
+                                                    <x-select-input id="unit-select"
+                                                        :class="$errors->has('form.unit') ? 'error select2' : 'select2'"
+                                                        wire:model="form.unit">
+                                                        <option value="">--Select Unit--</option>
+                                                        <option value="{{ $minutes }}">{{ $minutes }}</option>
+                                                        <option value="{{ $hours }}">{{ $hours }}</option>
+                                                    </x-select-input>
+                                                    @error('form.unit')
+                                                    <x-input-error :message="$message" />
+                                                    @enderror
+                                                </div>
+                                                <div class="col-md-4 inputs">
+                                                    <x-input type="text" name="dated" id="dated"
+                                                        placeholder="2020-09-23"
+                                                        :class="$errors->has('form.dated') ? 'error flatpickr-basic' : 'flatpickr-basic'"
+                                                        wire:model="form.dated" autocomplete="off" />
+                                                    @error('form.dated')
+                                                    <x-input-error :message="$message" />
+                                                    @enderror
+                                                </div>
+                                                <div class="col-md-2 inputs" style="margin-top: 9px">
+                                                    <x-input-checkbox type="checkbox" id="is-billable"
+                                                        name="is_billable" wire:model="form.is_billable"
+                                                        statusClass="form-check-success"
+                                                        :labelValue="__('Is Billable')" />
+                                                    @error('form.is_billable')
+                                                    <x-input-error :message="$message" />
+                                                    @enderror
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </form>
                             <!--/ Submit Chat form -->
                         </div>
@@ -98,7 +187,34 @@
                     <!--/ Main chat area -->
                 </div>
             </div>
-            {{-- </div> --}}
         </div>
     </div>
 </div>
+@script
+<script type="module">
+    $(document).ready(function () {
+            // Reinitialize select2 on uni dropdown
+            Livewire.on('unit-select', (data) => {
+                var $select = $('#unit-select');
+                // Clear existing selections
+                $select.val(null).trigger('change');
+                // Set the new selections
+                $select.val(data[0].formUnit).trigger('change');
+            });
+
+            $('#unit-select').on('change', function(event) {
+                @this.set('form.unit', $(this).val());
+            });
+
+            // Reinitialize flatpickr
+            Livewire.dispatch('flatpickr');
+            Livewire.on('reinitialize-dispatcher', () => {
+                $(document).ready(function () {
+                    Livewire.dispatch('flatpickr');
+                    Livewire.dispatch('feather-icons');
+                })
+            })
+        });
+</script>
+@endscript
+
