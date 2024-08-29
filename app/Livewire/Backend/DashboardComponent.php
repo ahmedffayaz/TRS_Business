eec734ae6959399dfd119bffa129b79fc0e92326
Arsalan Khan <arsalan.khan@therightsw.com>
2024-08-29 16:04:55 +0500

    set the design of dashboard

--- app/Livewire/Backend/DashboardComponent.php
+++ app/Livewire/Backend/DashboardComponent.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Backend;
 
+use App\Models\Task;
 use App\Models\User;
 use App\Models\Client;
 use App\Models\Country;
@@ -27,6 +28,15 @@ class DashboardComponent extends Component
     protected $user;
     public $clientCount = 0;
     public $countries;
+    public $tasks;
+    public $projects;
+    public string $search = '';
+    public string $columnName = 'created_at';
+    public string $sortDirection = 'desc';
+
+    public string $taskSearch = '';
+    public string $taskColumnName = 'created_at';
+    public string $taskSortDirection = 'desc';
     public $form = [
         'clientName' => '',
         'clientContryId' => '',
@@ -65,9 +75,9 @@ class DashboardComponent extends Component
         $this->taskCount = $this->getTaskCount();
         $this->clientDetail = $this->getClientDetail();
         $this->clientCount = $this->getClientCount();
+        $this->tasks = $this->getTasks();
 
         if ($this->clientDetail) {
-
             $this->form = [
                 'clientName' => $this->clientDetail->name,
                 'clientContryId' => $this->clientDetail->country->id ?? null,
@@ -83,6 +93,8 @@ class DashboardComponent extends Component
     {
         $this->dispatch('reinitialize-icons');
         $this->dispatch('select-container');
+        $this->tasks = $this->getTasks();
+        $this->projects = $this->getProjects();
         return view('livewire.backend.dashboard-component');
     }
 
@@ -142,7 +154,7 @@ class DashboardComponent extends Component
     {
         if($this->user->hasRole('client')) {
 
-            return Project::where('client_id', $this->user->client_id)->count();
+            return Project::where('client_id', $this->user->client_id)->where('business_id', $this->user->business_id)->count();
         }elseif($this->user->hasRole('admin')) {
 
             return Project::where('business_id', $this->user->business_id)->count();
@@ -218,5 +230,37 @@ class DashboardComponent extends Component
         $this->dispatch('close-main-modal');
     }
 
+    public function getTasks()
+    {
+        $user = auth()->user();
+        if($user->hasRole('client')) {
 
+            return Task::whereHas('project', function ($query) use ($user) {
+                $query->where('business_id', $user->business_id)
+                    ->where('client_id', $user->client_id);
+            })->with(['project', 'comments'])
+            ->where('completed_at',null)
+            ->whereBetween('end_date', [now()->subDays(15)->format('Y-m-d'), now()->addDays(15)->format('Y-m-d')])
+            ->getList($this->taskSearch, $this->taskColumnName, $this->taskSortDirection)
+            ->latest()->take(4)->get();
+       }
+        return null;
+    }
+
+    // #[On('get-projects')]
+    public function getProjects()
+    {
+        $user = auth()->user();
+        if($user->hasRole('client')) {
+            return Project::sessionBusiness()->when(!$user->hasRole('super-admin'), function ($query) use ($user) {
+                $query->whereHas('client', function ($query) use ($user) {
+                    $query->where('id', $user->client_id);
+                });
+            })->with(['client', 'members', 'tasks'])->whereIn('status',['in-progress','pending'])
+            ->whereBetween('end_date', [now()->subDays(15)->format('Y-m-d'), now()->addDays(15)->format('Y-m-d')])
+
+            ->getList($this->search, $this->columnName, $this->sortDirection)->withCount(['members', 'tasks'])->take(6)->get();
+        }
+        return null;
+    }
 }
