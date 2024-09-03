d0aa2c517b3487db530db1d5e2d60f0a1aed15cb
Arsalan Khan <arsalan.khan@therightsw.com>
2024-09-03 16:49:42 +0500

    resolve review comments

--- resources/views/livewire/backend/knowledge-base/knowledge-base-pdf.blade.php
+++ resources/views/livewire/backend/knowledge-base/knowledge-base-pdf.blade.php
@@ -125,9 +125,9 @@
             <div style="margin-bottom: 10px; color:#03096D;  margin-top: 30px;">
                 <h5 style="">{{ ucwords($categoryName) ?? 'null' }}</h5>
             </div>
-            <div style="overflow: hidden; margin-bottom: 20px; border: .2px solid #dcdcdc;border-radius: 3px;">
+            <div style="overflow: hidden; margin-bottom: 20px; border: 0.2px solid #dcdcdc;border-radius: 3px;">
                 @foreach ($knowledgeBase as $questionAnswer)
-                    <div style="padding: 10px; font-size: 12px; border-bottom: .2px solid #dcdcdc">
+                    <div style="padding: 10px; font-size: 12px; border-bottom: 0.2px solid #dcdcdc">
                        <div style="margin-bottom: 7px;"> {{ $questionAnswer->question }} </div>
                     <dive
                         style="font-size: 11px; color: #7F7F7F;">
